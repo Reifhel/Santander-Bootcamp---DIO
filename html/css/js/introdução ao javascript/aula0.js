6f84dfeff314d719b8fd94cd68816bfc5039679b
unknown <rafaelherdt1@gmail.com>
2022-06-20 08:35:21 -0300

    :p

---
+++ html/css/js/introdução ao javascript/aula0.js
@@ -0,0 +1,18 @@
+// comentário de uma linha
+
+/*
+comentário multiplas linhas
+*/
+
+// diferentemente das variaveis que são declaradas igual a maioria das linguagens, as
+// constantes são declaradas tudo em maiusculo e seus valores não podem ser alterados
+const PRECO = 2;
+var desconto = 0.2;
+var total = PRECO - 0.2;
+
+// funções -
+
+function soma(a,b){
+    console.log(a+b); // print no console
+    return a + b;
+}
