da57b249cdb50a236b11cc9360d96f00d9db6371
unknown <rafaelherdt1@gmail.com>
2022-06-20 10:55:41 -0300

    to do list

---
+++ html_css_js/introdução_ao_javascript/to-do-list/assets/js/script.js
@@ -0,0 +1,37 @@
+const formulario = document.getElementById('task-form');
+const taskList = document.getElementById('tasks');
+
+formulario.onsubmit = function (e){
+
+    e.preventDefault();
+    const inputField = document.getElementById('task-input');
+
+    if(inputField.value === ''){
+        console.log('tarefa vazia');
+    }
+    else{
+           addTask(inputField.value); 
+    }
+    formulario.reset();
+
+}
+
+function addTask(description) {
+	const taskContainer = document.createElement('div');
+	const newTask = document.createElement('input');
+	const taskLabel = document.createElement('label');
+	const taskDescriptionNode = document.createTextNode(description);
+
+	newTask.setAttribute('type', 'checkbox');
+	newTask.setAttribute('name', description);
+	newTask.setAttribute('id', description);
+
+	taskLabel.setAttribute('for', description);
+	taskLabel.appendChild(taskDescriptionNode);
+
+	taskContainer.classList.add('task-item');
+	taskContainer.appendChild(newTask);
+	taskContainer.appendChild(taskLabel);
+
+	taskList.appendChild(taskContainer);
+}
