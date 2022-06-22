88c8789fd83ab7e186f535554d461b7f267c1365
unknown <rafaelherdt1@gmail.com>
2022-06-21 22:08:45 -0300

    verificação contador

--- html_css_js/introdução_ao_javascript/contador/assets/js/scripts.js
+++ html_css_js/introdução_ao_javascript/contador/assets/js/scripts.js
@@ -1,12 +1,25 @@
 var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
 
+function verificar(){
+
+    if(currentNumber < 0){
+        currentNumberWrapper.style.color = 'red';
+    }
+    else{
+        currentNumberWrapper.style.color = 'white';
+    }
+
+}
+
 function increment(){
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber;
+    verificar();
 }
 
 function decrement(){
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
+    verificar();
 }
